7c471e482536c99a7a4caf35df464ad66ee2ed1f
Eric Moghioros <123109898+Mogalina@users.noreply.github.com>
2024-10-21 23:29:03 +0300

    Add custom fields separator for in-memory repository records.

--- src/repository/UserFileRepository.java
+++ src/repository/UserFileRepository.java
@@ -8,15 +8,36 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Repository class for managing User entities in a file-based/in-memory storage.
+ */
 public class UserFileRepository extends AbstractFileRepository<Long, User> {
 
-    public UserFileRepository(String fileName, Validator<User> validator) throws IOException {
+    private final String fieldsSeparator; // Custom fields separator
+
+    /**
+     * Constructs a new UserFileRepository with the specified file name and validator.
+     *
+     * @param fileName the name of the file used for data persistence
+     * @param validator the validator used to validate User entities
+     * @param fieldsSeparator the separator for the fields of the record
+     * @throws IOException if an error occurs while performing reading/writing operations on file
+     */
+    public UserFileRepository(String fileName, Validator<User> validator, String fieldsSeparator) throws IOException {
         super(fileName, validator);
+        this.fieldsSeparator = fieldsSeparator;
     }
 
+    /**
+     * Extracts user fields from a record and creates a User entity.
+     * The fields are separated using a specified custom delimiter.
+     *
+     * @param record the string record containing the user fields
+     * @return the User entity extracted from the record
+     */
     @Override
     protected User extractEntity(@NotNull String record) {
-        List<String> fields = Arrays.asList(record.split(","));
+        List<String> fields = Arrays.asList(record.split(fieldsSeparator));
         Long id = Long.parseLong(fields.get(0));
         String firstName = fields.get(1);
         String lastName = fields.get(2);
@@ -28,6 +49,12 @@ public class UserFileRepository extends AbstractFileRepository<Long, User> {
         return user;
     }
 
+    /**
+     * Converts the User entity to a string representation format for saving it to the specified file.
+     *
+     * @param user the User entity to be converted
+     * @return the string representation of the User entity
+     */
     @Override
     protected String entityToString(@NotNull User user) {
         return user.getId() +
