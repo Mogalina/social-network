41684c9bd75fed27e97e91d0d08f94f45e5caa05
Eric Moghioros <123109898+Mogalina@users.noreply.github.com>
2024-10-24 02:52:15 +0300

    Add User and Friendship services.

---
+++ src/service/FriendshipService.java
@@ -0,0 +1,19 @@
+package service;
+
+import models.Friendship;
+import repository.Repository;
+
+/**
+ * Specialized service class for performing operations on friendship relationships between users via repository.
+ */
+public class FriendshipService extends AbstractService<Long, Friendship> {
+
+    /**
+     * Constructs a new FriendshipService with the specified repository.
+     *
+     * @param repository the repository used to perform operations on persisting data
+     */
+    public FriendshipService(Repository<Long, Friendship> repository) {
+        super(repository);
+    }
+}
